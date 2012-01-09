635837c127b5b188889f4d776df19eb8e0c13b40
Oana Banu <banuoana.ct@gmail.com>
2012-01-09 12:19:57 +0200

    get_ads

--- casata-2/tabView/MapViewController.h
+++ casata-2/tabView/MapViewController.h
@@ -8,6 +8,8 @@
 
 #import <UIKit/UIKit.h>
 #import <MapKit/MapKit.h>
+#import "TRequest.h"
+#import "AppDelegate.h"
 
 @interface MapViewController : UIViewController <MKMapViewDelegate, UINavigationBarDelegate>{
     BOOL _doneInitialZoom;
@@ -15,6 +17,10 @@
     UINavigationItem *mapNavItem;
     //
     UISearchBar *searchBar;
+    double left,right,bottom,top;
+    TRequest *mapRequest;
+    BOOL hasLoadView;
+    AppDelegate *apdelegate;
    }
 @property (retain, nonatomic) IBOutlet MKMapView *mapView;
 @property (nonatomic,retain) UINavigationItem *mapNavItem;
@@ -24,5 +30,7 @@
 -(void)goHome;
 -(void)selectOptiuni;
 -(void)detaliiAnunt:(id)sender;
+-(void) showAdsFromData:(NSData *)data;
+-(void) getParamForReq;
 
 @end
